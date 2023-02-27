c11d0435d69bf61c6a9be0c2354516ad5184b3f4
Yury Stolarz <yurysscareer@gmail.com>
2023-02-27 20:19:27 -0300

    feat: add cors and errorHandle in application

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const fluxoComum = require('../routes/user.routes')
+const cors = require('cors');
+const routes = require('../routes');
+const errorHandler = require('../middleware/errorHandle');
 
 const app = express();
 
-// app.get('/coffee', (_req, res) => res.status(418).end());
-app.use('/login', fluxoComum)
+app.use(express.json());
+app.use(cors());
 
+app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use('/login', routes.userRoute);
+
+app.use(errorHandler);
 
 module.exports = app;
