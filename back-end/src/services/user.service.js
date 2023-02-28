2d084405085bb4b5da197fa2b93adeaa48f54a5e
Yury Stolarz <yurysscareer@gmail.com>
2023-02-28 15:20:32 -0300

    feat: add database in login service

--- back-end/src/services/user.service.js
+++ back-end/src/services/user.service.js
@@ -1,10 +1,25 @@
 const { User } = require('../database/models');
+const CustomError = require('./error/CustomError');
 const validations = require('./validations');
 
 const login = async ({ email, password }) => {
   validations.login(email, password);
 
-  return { message: 'voce esta logado' };
+  const user = await User.findOne({
+    where: {
+      email,
+    }
+  });
+
+  if (!user || user.password !== password) {
+    throw new CustomError('NOT_FOUND', 'Invalid email or password');
+  }
+
+  return {
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
 };
 
 module.exports = {
