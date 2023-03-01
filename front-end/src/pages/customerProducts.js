eb902f63ccb310568c4fb085933630cc2a522aa8
leandrojamir <leandrojamir@hotmail.com>
2023-03-01 15:05:31 -0300

    feat: setting up customer products page req11 and modifications on the router page for register req07

--- front-end/src/pages/customerProducts.js
+++ front-end/src/pages/customerProducts.js
@@ -1,7 +1,63 @@
-function customerProducts() {
+// 11 - Crie uma tela de produtos do cliente contendo uma barra de navegação - navbar -, que servirá também para demais telas das pessoas usuárias
+// Observações técnicas
+// Se oriente pela seguinte tela do protótipo: Comum / Produtos;
+import React from 'react';
+
+function CustomerProducts() {
   return (
-    <div>customer</div>
+    <div>
+      <nav>
+        {/* Elemento genérico que seja um item de menu para página de produtos; */}
+        <button
+          data-testid="customer_products__element-navbar-link-products"
+          type="button"
+        >
+          PRODUTOS
+
+        </button>
+        {/* Elemento genérico que seja um item de menu para página de pedidos; */}
+        <button
+          data-testid="customer_products__element-navbar-link-orders"
+          type="button"
+        >
+          MEUS PEDIDOS
+
+        </button>
+        {/* Elemento genérico para o nome da pessoa usuária; */}
+        <button
+          data-testid="customer_products__element-navbar-user-full-name"
+          type="button"
+        >
+          Nome da pessoa
+
+        </button>
+        {/* Elemento genérico que seja um item de menu para o logout. */}
+        <button
+          data-testid="customer_products__element-navbar-link-logout"
+          type="button"
+        >
+          Sair
+
+        </button>
+      </nav>
+      {/* describe(requirement(11), () => {
+  test("O avaliador testará a existência dos data-testids referentes ao navbar", async () => {
+    await expect(page).toFindElement(
+      customerProductsPage.element.navbar.links.products
+    );
+    await expect(page).toFindElement(
+      customerProductsPage.element.navbar.links.orders
+    );
+    await expect(page).toFindElement(
+      customerProductsPage.element.navbar.userFullName
+    );
+    await expect(page).toFindElement(
+      customerProductsPage.element.navbar.links.logout
+    );
+  });
+}); */}
+    </div>
   );
 }
 
-export default customerProducts;
+export default CustomerProducts;
--- front-end/src/pages/router.js
+++ front-end/src/pages/router.js
@@ -1,5 +1,8 @@
 import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
+// 11 - Crie uma tela de produtos do cliente contendo uma barra de navegação - navbar -, que servirá também para demais telas das pessoas usuárias
+import CustomerProducts from './customerProducts';
 import Login from './login';
+import Register from './register';
 
 function Router() {
   return (
@@ -8,6 +11,9 @@ function Router() {
         {/* A rota padrão (/) deve fazer redirecionamento para rota /login. */}
         <Route exact path="/" element={ <Navigate to="/login" /> } />
         <Route exact path="/login" element={ <Login /> } />
+        {/* incluindo para req07 não quebrou testes 01 a 05 */}
+        <Route path="/register" element={ <Register /> } />
+        <Route exact path="/customer/products" element={ <CustomerProducts /> } />
       </Routes>
     </BrowserRouter>
   );
