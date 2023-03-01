079d1c32eca103065cf99ca46313265f570fba3f
Yury Stolarz <yurysscareer@gmail.com>
2023-03-01 18:48:40 -0300

    refactor: metthod getAllProducts

--- back-end/src/services/product.service.js
+++ back-end/src/services/product.service.js
@@ -1,10 +1,6 @@
 const { Product } = require('../database/models');
 
-const getAllProducts = async () => {
-  const products = await Product.findAll();
-
-  return products;
-};
+const getAllProducts = async () =>  Product.findAll();
 
 module.exports = {
   getAllProducts,
