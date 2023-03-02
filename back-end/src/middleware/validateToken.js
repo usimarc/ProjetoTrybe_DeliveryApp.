1007cd5c6fe6049f26d338b9d79cefaaa3d47146
Yury Stolarz <yurysscareer@gmail.com>
2023-03-02 16:41:38 -0300

    fix: fixing call function verifyToken

--- back-end/src/middleware/validateToken.js
+++ back-end/src/middleware/validateToken.js
@@ -2,19 +2,23 @@ const CustomError = require('../services/error/CustomError');
 const { verifyToken } = require('../auth/token');
 
 module.exports = async (req, _res, next) => {
-  const token = req.header('Authorization');
+  try {
+    const token = req.header('Authorization');
 
-  if (!token) {
-    throw new CustomError('UNAUTHORIZED', 'Token not found');
+    if (!token) {
+      throw new CustomError('UNAUTHORIZED', 'Token not found');
+    }
+
+    const { error, data } = await verifyToken(token);
+
+    if (error) {
+      throw new CustomError('UNAUTHORIZED', 'Expired or invalid token');
+    }
+
+    req.user = data;
+
+    return next();
+  } catch (error) {
+    next(error);
   }
-
-  const { error, data } = verifyToken(token);
-
-  if (error) {
-    throw new CustomError('UNAUTHORIZED', 'Expired or invalid token');
-  }
-
-  req.user = data;
-
-  return next();
 };
