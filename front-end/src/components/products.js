e9d790cbc184769751878d76c88346fa45fb8908
mjggel <luismiguelvieira015@gmail.com>
2023-03-02 16:43:16 -0300

    feat: create card products with correct values

---
+++ front-end/src/components/products.js
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+
+function ProductCard({ props }) {
+  const { id, name, price, urlImage } = props;
+  const [quantity, setQuantity] = useState(0);
+
+  useEffect(() => {
+    if (quantity < 0) {
+      setQuantity(0);
+    }
+  }, [quantity]);
+
+  return (
+    <div key={ id }>
+      <p data-testid={ `customer_products__element-card-price-${id}` }>
+        {price.replace('.', ',')}
+      </p>
+      <img
+        data-testid={ `customer_products__img-card-bg-image-${id}` }
+        src={ urlImage }
+        alt={ name }
+        style={ { width: 100 } }
+      />
+      <p data-testid={ `customer_products__element-card-title-${id}` }>
+        {name}
+      </p>
+      <button
+        data-testid={ `customer_products__button-card-rm-item-${id}` }
+        type="button"
+        onClick={ (() => setQuantity(quantity - 1)) }
+      >
+        -
+
+      </button>
+      <input
+        type="number"
+        data-testid={ `customer_products__input-card-quantity-${id}` }
+        value={ quantity }
+      />
+      <button
+        data-testid={ `customer_products__button-card-add-item-${id}` }
+        type="button"
+        onClick={ (() => setQuantity(quantity + 1)) }
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+ProductCard.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  price: PropTypes.string,
+  urlImage: PropTypes.string,
+}.isRequired;
+
+export default ProductCard;
--- front-end/src/pages/customerProducts.js
+++ front-end/src/pages/customerProducts.js
@@ -1,97 +1,45 @@
-// 11 - Crie uma tela de produtos do cliente contendo uma barra de navegação - navbar -, que servirá também para demais telas das pessoas usuárias
-// Observações técnicas
-// Se oriente pela seguinte tela do protótipo: Comum / Produtos;
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { requestData } from '../ApiCall';
+import Navbar from '../components/navBar';
+import ProductCard from '../components/products';
 
 function CustomerProducts() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState('');
 
-  useEffect(() => (
-    setProducts()
-  ));
+  const setNameFunc = () => {
+    const getName = JSON.parse(localStorage.getItem('user'));
+    if (getName) {
+      setName(getName.name);
+    } else { setName('usuario'); }
+  };
+
+  useEffect(() => {
+    requestData('/products')
+      .then((result) => {
+        setProducts(result);
+      });
+    setNameFunc();
+  }, [setProducts]);
 
   return (
-    <div>
-      <nav>
-        {/* Elemento genérico que seja um item de menu para página de produtos; */}
-        <button
-          data-testid="customer_products__element-navbar-link-products"
-          type="button"
-        >
-          PRODUTOS
+    <>
+      <Navbar name={ name } />
+      {
+        products.map((element) => (
+          <ProductCard props={ element } key={ element.id } />
+        ))
+      }
+      <button
+        data-testid="customer_products__button-cart"
+        type="button"
+      >
+        <p data-testid="customer_products__checkout-bottom-value">
+          Ver Carrinho: R$
 
-        </button>
-        {/* Elemento genérico que seja um item de menu para página de pedidos; */}
-        <button
-          data-testid="customer_products__element-navbar-link-orders"
-          type="button"
-        >
-          MEUS PEDIDOS
-
-        </button>
-        {/* Elemento genérico para o nome da pessoa usuária; */}
-        <button
-          data-testid="customer_products__element-navbar-user-full-name"
-          type="button"
-        >
-          Nome da pessoa
-
-        </button>
-        {/* Elemento genérico que seja um item de menu para o logout. */}
-        <button
-          data-testid="customer_products__element-navbar-link-logout"
-          type="button"
-        >
-          Sair
-
-        </button>
-      </nav>
-      <div>
-        { products.map((element) => (
-          <div key={ element.id }>
-            <p data-testid={ `customer_products__element-card-price-${element.id}` }>
-              { element.price }
-            </p>
-            <img
-              data-testid={ `customer_products__img-card-bg-image-${element.id}` }
-              src={ element.urlImage }
-              alt={ element.name }
-            />
-            <p data-testid={ `customer_products__element-card-title-${element.id}` }>
-              { element.name }
-            </p>
-            <button
-              data-testid={ `customer_products__button-card-rm-item-${element.id}` }
-              type="button"
-            >
-              -
-
-            </button>
-            <input
-              data-testid={ `customer_products__input-card-quantity-${element.id}` }
-            />
-            <button
-              data-testid={ `customer_products__button-card-add-item-${element.id}` }
-              type="button"
-            >
-              +
-
-            </button>
-          </div>
-        ))}
-      </div>
-      <div>
-        <button
-          data-testid="customer_products__button-cart"
-          type="button"
-        >
-          <p data-testid="customer_products__checkout-bottom-value">
-            Ver Carrinho: R$
-
-          </p>
-        </button>
-      </div>
-    </div>
+        </p>
+      </button>
+    </>
   );
 }
 
