476c79923ce3e88a11f8d5cc03520bdb05325fd3
Yury Stolarz <yurysscareer@gmail.com>
2023-03-07 18:04:03 -0300

    feat: add route /seller/orders

--- front-end/src/routes/router.js
+++ front-end/src/routes/router.js
@@ -4,6 +4,7 @@ import Login from '../auth/login';
 import Register from '../auth/register';
 import CustomerCheckout from '../customers/pages/customerCheckout';
 import CustomerOrders from '../customers/pages/CustomerOrders';
+import SellerOrder from '../seller/pages/sellerOrder';
 
 function Router() {
   return (
@@ -12,6 +13,7 @@ function Router() {
       <Route exact path="/login" element={ <Login /> } />
       <Route exact path="/register" element={ <Register /> } />
       <Route exact path="/customer/products" element={ <CustomerProducts /> } />
+      <Route exact path="/seller/orders" element={ <SellerOrder /> } />
       <Route path="/customer/checkout" element={ <CustomerCheckout /> } />
       <Route path="/customer/sales" element={ <CustomerOrders /> } />
     </Routes>
